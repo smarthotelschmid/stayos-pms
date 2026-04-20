865e16fb31bd580835d90c8143e9af25bea82d16
root <root@euvds5017x12.startdedicated.net>
2026-04-20 10:01:10 +0000

    Portal: Check-in Flow nur bei isTest Buchungen

--- src/routes/portal.js
+++ src/routes/portal.js
@@ -131,6 +131,7 @@ router.get('/:token', async (req, res) => {
           : null,
         checkInFormCompleted: booking.checkInForm?.completed === true || booking.checkInCompleted === true,
         checkInCompleted: booking.checkInCompleted === true,
+        isTest: booking.isTest === true,
       },
     });
   } catch (err) {
